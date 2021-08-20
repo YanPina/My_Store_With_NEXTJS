6885d00d822831363689ceddd324a8639197bbf1
YanPina <yanpina85@gmail.com>
2021-08-20 00:06:32 -0300

    productfeed

--- yan_store/components/ProductFeed.js
+++ yan_store/components/ProductFeed.js
@@ -3,7 +3,8 @@ import Product from "./Product"
 function ProductFeed({ products }) {
     return (
         <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto '>
-            {products.map(({id, title, price, description, category, image}) => (
+            
+            {products.slice(0, 4).map(({id, title, price, description, category, image}) => (
                 <Product 
                     key={id}
                     id={id}
@@ -14,6 +15,34 @@ function ProductFeed({ products }) {
                     image={image}
                 />
             ))}
+
+            <img className='md:col-span-full' src="Banner/banner5.png" alt="" />
+        
+            <div className='md:col-span-2'>
+                {products.slice(4, 5).map(({id, title, price, description, category, image}) => (
+                    <Product 
+                        key={id}
+                        id={id}
+                        title={title}
+                        price={price}
+                        description={description}
+                        category={category}
+                        image={image}
+                    />
+                ))}
+            </div>
+
+            {products.slice(5, products.length).map(({id, title, price, description, category, image}) => (
+                    <Product 
+                        key={id}
+                        id={id}
+                        title={title}
+                        price={price}
+                        description={description}
+                        category={category}
+                        image={image}
+                    />
+                ))}
         </div>
     )
 }

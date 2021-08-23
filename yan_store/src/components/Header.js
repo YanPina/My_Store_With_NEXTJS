e3329f85d812da1d2c361f08ebfab577e5f9ff5e
YanPina <yanpina85@gmail.com>
2021-08-23 15:58:57 -0300

    add product in cart

--- yan_store/src/components/Header.js
+++ yan_store/src/components/Header.js
@@ -6,11 +6,16 @@ import {
 } from '@heroicons/react/outline';
 import { signIn, signOut, useSession } from 'next-auth/client';
 import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
+import { selectItems } from '../slices/cartSlice';
+
 
 function Header() {
 
     const [session] = useSession();
     const router = useRouter();
+    const items = useSelector(selectItems);
+
     return (
         <header>
             <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2'>
@@ -45,7 +50,9 @@ function Header() {
                     </div>
 
                     <div onClick={() => router.push('/checkout')} className='cursor-pointer relative link flex items-center'>
-                        <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-500 text-center rounded-full text-black font-bold'>0</span>
+                        <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-500 text-center rounded-full text-black font-bold'>
+                            {items.length}
+                        </span>
                         <ShoppingCartIcon className='h-10' />
                         <p className='hidden md:inline font-extrabold md:text-sm mt-2'>Cart</p>
                     </div>
--- yan_store/src/components/Product.js
+++ yan_store/src/components/Product.js
@@ -2,15 +2,27 @@ import Image from 'next/image'
 import { useState } from 'react';
 import { StarIcon } from '@heroicons/react/solid';
 import Currency from 'react-currency-formatter';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../slices/cartSlice';
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
 
 function Product({id, title, price, description, category, image}) {
-    
+
     const [rating] = useState(
         Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING);
 
+    const dispatch = useDispatch();
+
+    const addItemToCart = () => {
+        const product = {id, title, price, description, category, image};
+
+        //Adiciona o produto no carrinho
+        dispatch(addToCart(product))
+    };
+
+        
     return (
         <div className='relative flex flex-col m-5 bg-white z-30 p-10 '>
             <p className= 'absolute top-2 right-2 text-xs italic text-gray-400'>{category}</p>
@@ -33,7 +45,7 @@ function Product({id, title, price, description, category, image}) {
                 <Currency quantity={price} currency='USD' />
             </div>
 
-            <button className='mt-auto buttom'>Add to Cart</button>
+            <button onClick={addItemToCart} className='mt-auto buttom'>Add to Cart</button>
         </div>
     );
 }
